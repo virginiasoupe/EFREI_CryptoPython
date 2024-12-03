867ad1101556efa2e5364c55a0a0d4453f0e3f2d
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2024-12-03 10:18:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,16 +13,21 @@ def hello_world():
 
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    try:
+        valeur_bytes = valeur.encode()  # Conversion str -> bytes
+        token = f.encrypt(valeur_bytes)  # Encrypt la valeur
+        return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    except Exception as e:
+        return f"Erreur d'encryptage : {str(e)}"
 
 @app.route('/decrypt/<string:token>')
 def decryptage(token):
-    token_bytes = token.encode()  # Conversion str -> bytes
-    valeur_bytes = f.decrypt(token_bytes)  # Decrypt le token
-    return f"Valeur décryptée : {valeur_bytes.decode()}"  # Retourne la valeur en str
+    try:
+        token_bytes = token.encode()  # Conversion str -> bytes
+        valeur_bytes = f.decrypt(token_bytes)  # Decrypt le token
+        return f"Valeur décryptée : {valeur_bytes.decode()}"  # Retourne la valeur en str
+    except Exception as e:
+        return f"Erreur de décryptage : {str(e)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
-
