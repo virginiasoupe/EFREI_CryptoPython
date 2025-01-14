1b8fce0c2ce69eac19ddf4b8ae93315a6722a337
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2025-01-14 12:11:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,6 +18,10 @@ def maison():
 @app.route('/valet')
 def valet():
     return render_template('valet.html')
+
+@app.route('/chenille')
+def chenille():
+    return render_template('chenille.html')
     
 # Route pour chiffrer une valeur avec une clé privée manuelle
 @app.route('/encrypt/<string:key>/<string:valeur>')
